1cffa55819163671d044401f98f39f99a68dccfa
rahulkmehta <rkmehta@usc.edu>
2020-06-24 19:32:18 -0700

    fixes

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
 #### Author: Rahul Mehta
 #### Alpha Version: Started June 23, 2020
 #### Notes:
-1. Simpe value investing tool based on Greenblatt's book, TLB that Beats the Market.
-2. Financial data provided by finiviz.com.
+1. Simple 150-line script implementing value investment basics based on Greenblatt's book, TLB that Beats the Market.
+2. Companies were first screened based on market cap (2B+) — simple backtesting proved that micro (50M+) companies provided the possibility of a higher net return, but were too volatile in the short-term to consistently hold onto. Then, companies were ranked on two metrics. EBIT/EV and EBIT/Net Tangible Capital. 
+3. The top twenty companies are provided below.
+
+#### The Twenty Companies:
+1. USM
+2. BKNG
+3. WDFC
+4. APAM
+5. BIIB
+6. LOW
+7. MOH
+8. HPQ
+9. EVR
+10. AMED
+11. RHI
+12. COG
+13. PSA
+14. BBY
+15. NTAP
+16. SHLX
+17. FIZZ
+18. YETI
+19. PINC
+20. JBHT
+
+#### Acknowledgments
+1. Financial data provided by finiviz.com and financialmodelingprep.com.
+2. Documentation can be read https://financialmodelingprep.com/developer/docs/.
 
--- library.py
+++ library.py
@@ -14,16 +14,14 @@ from urllib.request import urlopen
 from finviz.screener import Screener
 from intrinio_sdk.rest import ApiException
 
-# [PRESCRIPTING DIRECTIVES & CONSTANT CREATION]
+# [PRE-SCRIPTING DIRECTIVES & CONSTANT CREATION]
 intrinio_sdk.ApiClient().configuration.api_key['api_key'] = 'Ojg1Y2IzMzFlNjcxY2U3YmYwNzc5MjgyNTgwMjEyM2Jh'
 company_api = intrinio_sdk.CompanyApi()
-tag = 'ebit'
 
-# [LOOK AT COMPANIES WITH MCAPS OVER 3B]
+# [PRIMARY SCREEN FOR COMPANIES WITH M_CAPS GREATER THAN 3B.]
 def compilepimary():
 	dictionary_ticker_mc = {}
 	filters = ['cap_midover','geo_usa']
-	print (filters)
 	stock_list = Screener(filters=filters, table='Valuation', order='market cap')  # Get the performance table and sort it by price ascending
 	for stock in stock_list:
 		dictionary_ticker_mc[stock['Ticker']] = stock['Market Cap']
@@ -32,15 +30,16 @@ def compilepimary():
 		del dictionary_ticker_mc[k]
 	return dictionary_ticker_mc
 
+# [PULLS INFORMATION ABOUT EBIT AND ENTERPROSE VALUE. CALCULATE RATIO AND SAVES TO PICKLE FILE]
 def first_rank(mydict):
 	ebitev_dict = {}
 	for k,v in mydict.items():
-		#[OPEN UP INCOME STATEMENT]
+		#[OPENS UP INCOME STATEMENT]
 		response = urlopen("https://financialmodelingprep.com/api/v3/income-statement/" + k + "?apikey=81ff40df1c59cd9cfc42b91bafe21a9a")
 		data = response.read().decode("utf-8")
 		incomedict = json.loads(data)
 
-		#[OPEN UP VALUATION MULTIPLES]
+		#[OPEN SUP VALUATION MULTIPLES]
 		ev_response = urlopen("https://financialmodelingprep.com/api/v3/enterprise-values/" + k + "?apikey=81ff40df1c59cd9cfc42b91bafe21a9a")
 		dataev = ev_response.read().decode("utf-8")
 		evdict = json.loads(dataev)
@@ -53,18 +52,96 @@ def first_rank(mydict):
 					ev = evdict[0]['enterpriseValue']
 					ebitev_dict[k] = ebit/ev
 				else:
-					print ("DATES ARE NOT THE SAME")
+					print ("DATES ARE NOT THE SAME IN REPORTS.")
 			except:
-				print (incomedict)
-				print ("")
-				print (evdict)
+				print (incomedict, " ", evdict)
 		else:
-			print (k)
+			print ("COULDN'T FIND A REPORT FOR: " + k)
+
+	# [DUMPS FILE]
 	with open('ebitev.pickle', 'wb') as f:
 		pickle.dump(ebitev_dict, f, pickle.HIGHEST_PROTOCOL)
 
+# [OPENS PICKLED FILE AND RANKS. RETURNS THE ARRAY OF TUPLES]
+def first_numbering():
+	rank_tupled = []
+	with open ('ebitev.pickle', 'rb') as f:
+		data = pickle.load(f)
+	counter = 1;
+	for k,v in sorted(data.items(), key = lambda item: item[1]):
+		rank_tupled.append(((k,v), counter))
+		counter+=1
+
+	with open('rankedonebitev.pickle', 'wb') as f:
+		pickle.dump(rank_tupled, f, pickle.HIGHEST_PROTOCOL)
+
+# [TAKES IN RANKED ARRAY AND PARSES THE REMAINING RESULTS FOR ROCE. SAVES ROCE RESULTS TO PICKLED FILE]
+def second_rank():
+	# [OPENING UP PICKLED FILE]
+	ebitroce_dict = {}
+	with open ('rankedonebitev.pickle', 'rb') as f:
+		arr = pickle.load(f)
+
+	for item in arr:
+		#[OPEN UP VALUATION MULTIPLES]
+		roce_response = urlopen("https://financialmodelingprep.com/api/v3/ratios/" + item[0][0] + "?apikey=81ff40df1c59cd9cfc42b91bafe21a9a")
+		dataroce = roce_response.read().decode("utf-8")
+		rocedict = json.loads(dataroce)
+
+		#[CALCULATIONS]
+		if (len(rocedict) > 0):
+			try: 
+				if rocedict[0]['returnOnCapitalEmployed'] != None:
+					ebitroce_dict[item[0][0]] = rocedict[0]['returnOnCapitalEmployed']
+			except:
+				print ("AN EXCEPTION OCCURRED")
+		else: 
+			print ("COULDNT FIND A ROCE MULTIPLE")
+
+	#[DUMPS IT INTO PICKLE FILE]
+	with open('ebitroce.pickle', 'wb') as file:
+		pickle.dump(ebitroce_dict, file, pickle.HIGHEST_PROTOCOL)
+
+# [OPENS PICKLED FILE AND RANKS. RETURNS THE ARRAY OF TUPLES]
+def second_numbering():
+	rank_tupled = []
+	with open ('ebitroce.pickle', 'rb') as f:
+		data = pickle.load(f)
+
+	counter = 1
+	for k,v in sorted(data.items(), key = lambda item: item[1]):
+		rank_tupled.append(((k,v), counter))
+		counter += 1
+
+	with open('rankedonebitroce.pickle', 'wb') as file:
+		pickle.dump(rank_tupled, file, pickle.HIGHEST_PROTOCOL)
+
+# [OPENS UP THE TWO RANKED PICKLED FILES AND COMBINES THE RANKING]
+def final_numbering():
+	final_rank = []
+	with open ('rankedonebitev.pickle', 'rb') as f:
+		ebitev = pickle.load(f)
+	with open('rankedonebitroce.pickle', 'rb') as file:
+		ebitroce = pickle.load(file)
+
+	for item in ebitev:
+		ebit_rank = item[1]
+		for entry in ebitroce:
+			if (entry[0][0] == item[0][0]):
+				roce_rank = entry[1]
+				final_rank.append((item[0][0], ebit_rank + roce_rank))
+
+	final_rank.sort(key = lambda x:x[1])
+	for i in range(0, 20):
+		print (final_rank[len(final_rank)-(i+1)][0])
+
+#[DRIVER]
 def testmain():
-	primary_dict = compilepimary()
-	first_rank(primary_dict)
+	# primary_dict = compilepimary()
+	# first_rank(primary_dict)
+	# first_numbering()
+	# second_rank()
+	# second_numbering()
+	final_numbering()
 
 testmain()
